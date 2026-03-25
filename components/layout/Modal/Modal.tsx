d3a1c0d9330759e193209fb52e1e8b4ffde819e7
Mateusz Matecki <mateusz.matecki@synteco.pl>
2026-03-25 13:19:06 +0100

    init

---
+++ components/layout/Modal/Modal.tsx
@@ -0,0 +1,31 @@
+import {
+  Breakpoint,
+  Dialog,
+  DialogContent,
+  DialogContentProps,
+  SxProps,
+} from "@mui/material";
+import { ReactNode } from "react";
+
+type Props = {
+  open: boolean;
+  handleClose: () => void;
+  children: ReactNode;
+  size?: Breakpoint;
+};
+
+export default function Modal({
+  open,
+  children,
+  size = "sm",
+  handleClose,
+  ...props
+}: Props & Partial<DialogContentProps>) {
+  return (
+    <Dialog open={open} maxWidth={size} fullWidth onClose={handleClose}>
+      <DialogContent sx={{ p: 3.5 }} {...props}>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+}
